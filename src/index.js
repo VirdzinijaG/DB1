ec2b2b4b084699c923ebc6c6450be96d1b9d4a3d
Virdžinija <virdzinija.cikanaviciute@gmail.com>
2021-08-20 18:46:11 +0300

    homework

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
-console.log("Veikia");
+/*
+meniu:
+
+1. zmoniu sarasas
+2. prideti zmogu
+3. pakeisti zmogaus info
+4. istrinti zmogu
+0. baigti
+
+1. is DB gauti visa zmoniu sarasa ir atspausdinti i ekrana
+2.
+    paklausti vardo, pavardes ir algos
+    irasyti nauja irasa i zmoniu lentele
+3. 
+    paklausti id,
+    paklausti vardo, pavardes ir algos
+    pakeisti irasa zmoniu lenteleje su nurodytu id
+4. 
+    paklausti id,
+    is zmoniu lenteles istrinti irasa su nurodytu id
+0. baigti darba
+
+*/
+
