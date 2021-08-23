cb06a70a9d98350da754352be4766a446b079819
Virdžinija <virdzinija.cikanaviciute@gmail.com>
2021-08-23 20:53:27 +0300

    people list, connection database

--- src/index.js
+++ src/index.js
@@ -38,41 +38,46 @@ function inputText(msg) {
     });
 }
 
-const conn = mysql.createConnection({
-    host: "localhost",
-    user: "nodejs",
-    password: "nodejs123456",
-    database: "zmones",
-    multipleStatements: true,
-});
+// connect pakeitimas, kad leistu prisijungti kelis kartus
 
-function dbConnect() {
+function dbConnect() { // prisijungimas prie duomenu bazes
+    const conn = mysql.createConnection({ // kintamasis sukurtas funkcijoje
+        host: "localhost",
+        user: "nodejs",
+        password: "nodejs123456",
+        database: "zmones",
+        multipleStatements: true,
+    });
     return new Promise((resolve, reject) => {
         conn.connect((err) => {
             if (err) {
                 reject(err);
                 return;
             }
-            resolve();
+            resolve(conn);
         });
     });
 }
 
-function dbDisconnect() {
-    return new Promise((resolve, reject) => {
-        conn.end((err) => {
-            if (err) {
-                reject(err);
-                return;
-            }
-            resolve();
+function dbDisconnect(conn) {  // objektas, kuri norim atjungti
+    if (conn) {
+        return new Promise((resolve, reject) => {
+            conn.end((err) => {
+                if (err) {
+                    reject(err);
+                    return;
+                }
+                resolve();
+            });
         });
-    });
+    } else {
+        return Promise.resolve();
+    }
 }
 
-function dbQuery() {
+function dbQuery(conn, ...args) { // pirmas parametras connection, antras masyvas args 
     return new Promise((resolve, reject) => {
-        conn.query(...arguments, (err, results, fields) => {
+        conn.query(...args, (err, results, fields) => {
             if (err) {
                 reject(err);
                 return;
@@ -101,31 +106,33 @@ function printTable(r) {
 }
 
 let run = true;
-while (run) {
-    console.log(`
+while (run) { // meniu atspausdinamas // su backtick spaudina per kelias eilutes
+    console.log(` 
 1. zmoniu sarasas
 2. prideti zmogu
 3. pakeisti zmogaus info
 4. istrinti zmogu
 0. baigti
     `);
-    let pasirinkikmas = await inputText("Pasirink: ");
-    pasirinkikmas = parseInt(pasirinkikmas);
+    let pasirinkikmas = await inputText("Pasirink: "); // pasirinkimo nustatytmas
+    pasirinkikmas = parseInt(pasirinkikmas); // padaromas skaicius
     switch (pasirinkikmas) {
         case 1:
+            let conn;
             try {
-                await dbConnect();
-                let r = await dbQuery("select id, vardas, pavarde, gim_data, alga from zmones");
-                printTable(r);
+                conn = await dbConnect(); // prisijungimas prie duomenu bazes
+                let r = await dbQuery(conn, "select id, vardas, pavarde, gim_data, alga from zmones"); // nusiunciama uzklausa
+                printTable(r); // gautas rezultatas atspausdinamas
             }
             catch (err) {
                 console.log("Klaida: ", err);
             } finally {
                 try {
-                    await dbDisconnect();
+                    await dbDisconnect(conn);
                 } catch (err) {
                 }
             }
+            break;
         case 2:
             let vardas = await inputText("Ivesk varda: ");
             let pavarde = await inputText("Ivesk pavarde: ");
@@ -147,13 +154,16 @@ while (run) {
             } else {
                 console.log("Blogai ivesti duomenys");
             }
+            break;
 
         case 3:
+            break;
         case 4:
-        case 0:
+            break;
+        case 0: // baigia darba
             run = false;
             break;
-        default:
+        default: // ivedus netinkama pasirinkima
             console.log("Ismok naudotis klaviatura");
 
     }
